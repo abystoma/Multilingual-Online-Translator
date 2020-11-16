26d3559c11fa37ee8f64ba195fdcaff72940d617
FlukeHermit <zayed.humayun@g.bracu.ac.bd>
2020-11-16 19:42:22 +0600

    Init Commit

---
+++ translator.py
@@ -0,0 +1,5 @@
+language = input(
+    'Type "en" if you want to translate from French into English, '
+    'or "fr" if you want to translate from English into French:\n')
+word = input('Type the word you want to translate:\n')
+print(f'You chose "{language}" as the language to translate "{word}" to.')
