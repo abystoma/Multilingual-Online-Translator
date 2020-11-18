e1a7e3c2b907616bff4d8cebe7eac75ff8da7780
FlukeHermit <zayed.humayun@g.bracu.ac.bd>
2020-11-18 21:03:37 +0600

    Added option to translate all languages

--- translator.py
+++ translator.py
@@ -1,5 +1,8 @@
-import requests
+from requests import Session
+
 from bs4 import BeautifulSoup
+
+session = Session()
 
 languages = [None,
              "Arabic",
@@ -32,38 +35,58 @@ print("""Hello, you're welcome to the translator. Translator supports:
 12. Russian
 13. Turkish""")
 
-src_lang = int(input("Type the number of your language: "))
-lang_in = languages[src_lang].lower()
-trg_lang = int(input("Type the number of language you want to translate to: "))
-lang_out = languages[trg_lang].lower()
 
+def to_lang(word, lang_in, lang_out):
+    url = f"https://context.reverso.net/translation/{lang_in.lower()}-{lang_out.lower()}/{word}"
+
+    r = session.get(url, headers={'user-agent': 'Mozilla/5.0'})
+
+    # created a parse for the page
+    soup = BeautifulSoup(r.content, 'html.parser')
+
+    translations = [i.text.strip()
+                    for i in soup.find_all("a", {'class': 'translation'})][1::]
+
+    src_examples = [i.text.strip()
+                    for i in soup.find_all("div", {'class': 'src ltr'})]
+
+    trg_examples = [i.text.strip()
+                    for i in soup.find_all("div", {'class': ['trg ltr', 'trg rtl arabic', 'trg rtl']})]
+
+    output = ""
+
+    output += f"{lang_out.title()} Translations:\n"
+
+    output += "\n".join(translations[:1])
+    output += "\n"
+    output += f"\n{lang_out.title()} Example:\n"
+
+    for src, trg in zip(src_examples[:1], trg_examples[:1]):
+        output += src+":\n"+trg+"\n"
+
+    output += "\n\n"
+    
+    return output
+
+
+src_lang = int(input("Type the number of your language: "))
+lang_in = languages[src_lang]
+trg_lang = int(input("Type the number of language you want to translate to: "))
+lang_out = languages[trg_lang]
 word = input("Type the word you want to translate: ")
 
-url = f"https://context.reverso.net/translation/{lang_in}-{lang_out}/{word}"
 
+if lang_out is None:
+    output = ""
+    for language in languages[1::]:
+        if language != lang_in:
+            output += to_lang(word, lang_in, language)
+    print(output)
+    with open(f'{word}.txt', 'a', encoding="utf-8") as file:
+        file.write(output)
 
-r = requests.get(url, headers={'user-agent': 'Mozilla/5.0'})
-
-# created a parse for the page
-soup = BeautifulSoup(r.content, 'html.parser')
-
-translations = [i.text.strip()
-                for i in soup.find_all("a", {'class': 'translation'})][1::]
-
-src_examples = [i.text.strip()
-                for i in soup.find_all("div", {'class': 'src ltr'})]
-
-trg_examples = [i.text.strip()
-                for i in soup.find_all("div", {'class': 'trg ltr'})]
-print()
-print(f"{lang_out.title()} Translations:")
-
-for translation in translations[:5]:
-    print(translation)
-
-print()
-print(f"{lang_out.title()} Examples:")
-
-for src, trg in zip(src_examples[:5], trg_examples[:5]):
-    print(src+":\n"+trg)
-    print()
+else:
+    output = to_lang(word, lang_in, lang_out)
+    print(output)
+    with open(f'{word}.txt', 'w', encoding="utf-8") as file:
+        file.write(output)
